8d49f4532aaabf0f7b416f5413dddfd24b684292
Cristian Almendro Lazarte <cristian.mal.90@gmail.com>
2023-08-10 17:22:40 -0400

    remove login screen and dependencies

--- application/MainApp.tsx
+++ application/MainApp.tsx
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import PokemonList from './PokemonList';
-import Login from './Login';
 import PokemonDetail from './PokemonDetail';
 
 const Stack = createNativeStackNavigator();
@@ -20,7 +19,6 @@ function MainApp() {
         <Stack.Navigator
           screenOptions={{headerShown: false}}
           initialRouteName="PokemonList">
-          <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="PokemonList" component={PokemonList} />
           <Stack.Screen name="PokemonDetail" component={PokemonDetail} />
         </Stack.Navigator>
