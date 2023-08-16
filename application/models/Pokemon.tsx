ec8bd9ea1cd35151576c0cd1162d31105611c0ec
Cristian Almendro Lazarte <cristian.mal.90@gmail.com>
2023-08-16 17:54:57 -0400

    refactor code

--- application/models/Pokemon.tsx
+++ application/models/Pokemon.tsx
@@ -1,43 +1,20 @@
-export interface PokemonType {
-  id: number;
-  name: string;
-  pokemon_v2_type: PokemonType;
+interface PokemonType {
+  type: {
+    id: number;
+    name: string;
+  };
 }
 
-export interface GenerationData {
-  id: number;
-  name: string;
+interface Generation {
+  generation: {
+    id: number;
+    name: string;
+  };
 }
 
 export interface PokemonData {
   id: number;
   name: string;
-  height: number;
-  weight: number;
-  types: PokemonType[];
-  generationData: {
-    generation: GenerationData;
-  };
-}
-
-export class Pokemon {
-  id: number;
-  name: string;
-  height: number;
-  weight: number;
-  types: PokemonType[];
-  generationData: GenerationData;
-
-  constructor(data: PokemonData) {
-    this.id = data.id;
-    this.name = data.name;
-    this.height = data.height;
-    this.weight = data.weight;
-    this.types = data.types.map(type => ({
-      id: type.pokemon_v2_type.id,
-      name: type.pokemon_v2_type.name,
-      pokemon_v2_type: type,
-    }));
-    this.generationData = data.generationData.generation;
-  }
+  pokemonTypes: PokemonType[];
+  specy: Generation;
 }
